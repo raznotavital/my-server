677e7061f7a655ceca1e3e3eaa76e0d816137e70
raznotavital <raznotavital@gmail.com>
2025-06-05 17:13:20 +0300

    Update users.js

--- api/users.js
+++ api/users.js
@@ -1,12 +1,9 @@
-export default async function handler(req, res) {
-  try {
-    // כאן תשים את הכתובת של השרת המקומי שלך
-  import users from './users.json';
+const users = require('./users.json');
 
-export default function handler(req, res) {
-  res.status(200).json(users);
-}
+module.exports = async (req, res) => {
+  try {
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch users' });
   }
-}
+};
